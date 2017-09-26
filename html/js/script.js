c289aaeca4ae6ce2fc96a08d5d9f12fb417833a8
Hampus Lidin <lidin@student.chalmers.se>
2017-09-26 13:27:36 +0000

    Add landscape and rover
    Also change the way the camera follows the rover by making the camera
    entity a child of the rover entity. The sky box has to be at the
    same level as the camera for some reason, so also make the sky box a
    child to the rover.

--- html/js/script.js
+++ html/js/script.js
@@ -23,17 +23,16 @@ AFRAME.registerComponent("follow", {
 
     tick: function (time, timeDelta) {
         var directionVec3 = new THREE.Vector3();
-        var el = this.el;
         var data = this.data;
 
         // Get the target's position
         var targetPosition = data.target.object3D.position;
 
         // Set this element's position to the target's position
-        el.setAttribute('position', {
+        this.el.setAttribute('position', {
             x: targetPosition.x,
-            y: targetPosition.y + 1.6,
-            z: targetPosition.z
+            y: targetPosition.y + 0.75,
+            z: targetPosition.z - 0.25
         });
     },
 });
