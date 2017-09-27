1e4db71a236e3c5cf583bd3c4a92d6d388d514f8
Jonas Arvidsson <jonas.arvidsson0@gmail.com>
2017-09-27 10:39:33 +0200

    added movement controls

--- html/js/script.js
+++ html/js/script.js
@@ -36,3 +36,73 @@ AFRAME.registerComponent("follow", {
         });
     },
 });
+
+
+
+
+
+
+var up = false,
+    right = false,
+    down = false,
+    left = false;
+
+
+document.addEventListener('keydown',press)
+function press(e){
+  if (e.keyCode === 38 /* up */ || e.keyCode === 87 /* w */ || e.keyCode === 90 /* z */){
+    up = true
+  }
+  if (e.keyCode === 39 /* right */ || e.keyCode === 68 /* d */){
+    right = true
+  }
+  if (e.keyCode === 40 /* down */ || e.keyCode === 83 /* s */){
+    down = true
+  }
+  if (e.keyCode === 37 /* left */ || e.keyCode === 65 /* a */ || e.keyCode === 81 /* q */){
+    left = true
+  }
+}
+document.addEventListener('keyup',release)
+function release(e){
+  if (e.keyCode === 38 /* up */ || e.keyCode === 87 /* w */ || e.keyCode === 90 /* z */){
+    up = false
+  }
+  if (e.keyCode === 39 /* right */ || e.keyCode === 68 /* d */){
+    right = false
+  }
+  if (e.keyCode === 40 /* down */ || e.keyCode === 83 /* s */){
+    down = false
+  }
+  if (e.keyCode === 37 /* left */ || e.keyCode === 65 /* a */ || e.keyCode === 81 /* q */){
+    left = false
+  }
+}
+
+
+
+function gameLoop(){
+  var rover = document.getElementById('rover').object3D;
+
+  if (up){
+  rover.translateZ( -0.1 );
+  }
+  if(down){
+      rover.translateZ(0.1);
+  }
+  if(left){
+      rover.rotateY(0.1);
+  }
+  if(right){
+      rover.rotateY(-0.1);
+  }
+    
+
+
+requestAnimationFrame(gameLoop)
+}
+ requestAnimationFrame(gameLoop)
+
+
+
+
