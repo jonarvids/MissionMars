b58024e1e1b77b279b711e3668ef674ca1106e94
Hampus Lidin <lidin@student.chalmers.se>
2017-10-12 17:35:42 +0200

    Small changes

--- html/app.js
+++ html/app.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var path = require('path');
+let express = require('express');
+let app = express();
+let server = require('http').Server(app);
+let io = require('socket.io')(server);
+let path = require('path');
 let pyshell = require('python-shell');
 
 //------------------------------------//
