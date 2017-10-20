946655fab84baa8779e8aca05dfe6a3dabd4c570
Jonas Arvidsson <jonas.arvidsson0@gmail.com>
2017-10-20 17:42:13 +0200

    added win/lose screen and changed font

--- html/js/gui-script.js
+++ html/js/gui-script.js
@@ -1,117 +1,150 @@
 window.onload = () => {
-	// Controls
-	let up, down, left, right;
-	let roverHealth, roverBattery;
-	
-	// Elements
-	const hpText        = document.getElementById("hpText");
-	const hpMeter       = document.getElementById("hpMeter");
-	const batteryText   = document.getElementById("batteryText");
-	const batteryMeter  = document.getElementById("batteryMeter");
-	const roverImages   = document.getElementById("roverImages");
-	const triangleUp    = document.getElementById("triangleUp");
-	const triangleDown  = document.getElementById("triangleDown");
-	const triangleLeft  = document.getElementById("triangleLeft");
-	const triangleRight = document.getElementById("triangleRight");
-	
-	const socket = io.connect('http://localhost');
+    // Controls
+    let up, down, left, right;
+    let roverHealth, roverBattery;
 
-	socket.connect('http://localhost').on('roverData', (data) => {
-		roverHealth = data.health;
-		roverBattery = data.battery;
-	
-		// HP
-		hpText.innerHTML = Math.round(roverHealth).toString() + "%";
-		hpMeter.style.width = roverHealth.toString() + "%";
-	
-		// Battery
-		batteryText.innerHTML = Math.round(roverBattery).toString() + "%";
-		batteryMeter.style.width = roverBattery.toString() + "%";
-		
-		// Rover
-		if (roverHealth >= 80) {
-			roverImages.src = "assets/images/roverImage.png";
-		} else if (roverHealth >= 60) {
-			roverImages.src = "assets/images/roverImage_slightly_damaged.png";
-		} else if (roverHealth >= 40) {
-			roverImages.src = "assets/images/roverImage_damaged.png";
-		} else if (roverHealth >= 20) {
-			hpMeter.classList.add('bg-warning');
-			hpMeter.classList.remove('bg-success');
-			roverImages.src = "assets/images/roverImage_critical.png";
-		} else if (roverHealth > 0) {
-			hpMeter.classList.add('bg-danger');
-			hpMeter.classList.remove('bg-warning');
-		} else {
-			roverImages.src = "assets/images/roverImage_dead.png";
-		}
-		
-		if (roverBattery < 33) {
-			roverBattery.classList.add('bg-danger');
-		}
-	});
-	
-	document.addEventListener('keydown', (e) => {
-		if (e.keyCode === 38 /* up */ || 
-				e.keyCode === 87 /* w */ || 
-				e.keyCode === 90 /* z */ ) {
-			triangleUp.style.borderColor = "red";
-			up = true;
-			socket.emit('control', { direction: 'Up', pressed: true });
-		}
-		
-		if (e.keyCode === 39 /* right */ || 
-				e.keyCode === 68 /* d */ ) {
-			triangleRight.style.borderColor = "red";
-			right = true;
-			socket.emit('control', { direction: 'Right', pressed: true });
-		}
-		
-		if (e.keyCode === 40 /* down */ || 
-				e.keyCode === 83 /* s */ ) {
-			triangleDown.style.borderColor = "red";
-			back = true;
-			socket.emit('control', { direction: 'Down', pressed: true });
-		}
-	
-		if (e.keyCode === 37 /* left */ || 
-				e.keyCode === 65 /* a */ || 
-				e.keyCode === 81 /* q */ ) {
-			triangleLeft.style.borderColor = "red";
-			left = true;
-			socket.emit('control', { direction: 'Left', pressed: true });
-		}
-	});
-	
-	document.addEventListener('keyup', (e) => { 
-		if (e.keyCode === 38 /* up */ || 
-				e.keyCode === 87 /* w */ || 
-				e.keyCode === 90 /* z */ ) {
-			triangleUp.style.borderColor = "white";
-			up = false;
-			socket.emit('control', { direction: 'Up', pressed: false });
-		}
-	
-		if (e.keyCode === 39 /* right */ ||
-				e.keyCode === 68 /* d */ ) {
-			triangleRight.style.borderColor = "white";
-			right = false;
-			socket.emit('control', { direction: 'Right', pressed: false });
-		}
-	
-		if (e.keyCode === 40 /* down */ ||
-				e.keyCode === 83 /* s */ ) {
-			triangleDown.style.borderColor = "white";
-			down = false;
-			socket.emit('control', { direction: 'Down', pressed: false });
-		}
-	
-		if (e.keyCode === 37 /* left */ || 
-				e.keyCode === 65 /* a */ || 
-				e.keyCode === 81 /* q */ ) {
-			triangleLeft.style.borderColor = "white";
-			left = false;
-			socket.emit('control', { direction: 'Left', pressed: false });
-		}
-	});
+    // Elements
+    const hpText = document.getElementById("hpText");
+    const hpMeter = document.getElementById("hpMeter");
+    const batteryText = document.getElementById("batteryText");
+    const batteryMeter = document.getElementById("batteryMeter");
+    const roverImages = document.getElementById("roverImages");
+    const triangleUp = document.getElementById("triangleUp");
+    const triangleDown = document.getElementById("triangleDown");
+    const triangleLeft = document.getElementById("triangleLeft");
+    const triangleRight = document.getElementById("triangleRight");
+    const winloseText = document.getElementById("winloseText");
+
+    const socket = io.connect('http://localhost');
+
+    socket.connect('http://localhost').on('roverData', (data) => {
+        roverHealth = data.health;
+        roverBattery = data.battery;
+
+        // HP
+        hpText.innerHTML = Math.round(roverHealth).toString() + "%";
+        hpMeter.style.width = roverHealth.toString() + "%";
+
+        // Battery
+        batteryText.innerHTML = Math.round(roverBattery).toString() + "%";
+        batteryMeter.style.width = roverBattery.toString() + "%";
+
+        // Rover
+        if (roverHealth >= 80) {
+            roverImages.src = "assets/images/roverImage.png";
+        } else if (roverHealth >= 60) {
+            roverImages.src = "assets/images/roverImage_slightly_damaged.png";
+        } else if (roverHealth >= 40) {
+            roverImages.src = "assets/images/roverImage_damaged.png";
+        } else if (roverHealth >= 20) {
+            hpMeter.classList.add('bg-warning');
+            hpMeter.classList.remove('bg-success');
+            roverImages.src = "assets/images/roverImage_critical.png";
+        } else if (roverHealth > 0) {
+            hpMeter.classList.add('bg-danger');
+            hpMeter.classList.remove('bg-warning');
+        } else {
+            roverImages.src = "assets/images/roverImage_dead.png";
+        }
+
+        if (roverBattery < 33) {
+            roverBattery.classList.add('bg-danger');
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.keyCode === 38 /* up */ ||
+            e.keyCode === 87 /* w */ ||
+            e.keyCode === 90 /* z */ ) {
+            triangleUp.style.borderColor = "red";
+            up = true;
+            socket.emit('control', {
+                direction: 'Up',
+                pressed: true
+            });
+
+            //Test code to open modal please remove
+            winloseText.style.color = "red";
+            winloseText.innerHTML = "UPPDRAG MISSLYCKAT";
+
+            $('#myModal').modal('show')
+
+
+        }
+
+        if (e.keyCode === 39 /* right */ ||
+            e.keyCode === 68 /* d */ ) {
+            triangleRight.style.borderColor = "red";
+            right = true;
+            socket.emit('control', {
+                direction: 'Right',
+                pressed: true
+            });
+        }
+
+        if (e.keyCode === 40 /* down */ ||
+            e.keyCode === 83 /* s */ ) {
+            triangleDown.style.borderColor = "red";
+            back = true;
+            socket.emit('control', {
+                direction: 'Down',
+                pressed: true
+            });
+        }
+
+        if (e.keyCode === 37 /* left */ ||
+            e.keyCode === 65 /* a */ ||
+            e.keyCode === 81 /* q */ ) {
+            triangleLeft.style.borderColor = "red";
+            left = true;
+            socket.emit('control', {
+                direction: 'Left',
+                pressed: true
+            });
+        }
+    });
+
+    document.addEventListener('keyup', (e) => {
+        if (e.keyCode === 38 /* up */ ||
+            e.keyCode === 87 /* w */ ||
+            e.keyCode === 90 /* z */ ) {
+            triangleUp.style.borderColor = "white";
+            up = false;
+            socket.emit('control', {
+                direction: 'Up',
+                pressed: false
+            });
+        }
+
+        if (e.keyCode === 39 /* right */ ||
+            e.keyCode === 68 /* d */ ) {
+            triangleRight.style.borderColor = "white";
+            right = false;
+            socket.emit('control', {
+                direction: 'Right',
+                pressed: false
+            });
+        }
+
+        if (e.keyCode === 40 /* down */ ||
+            e.keyCode === 83 /* s */ ) {
+            triangleDown.style.borderColor = "white";
+            down = false;
+            socket.emit('control', {
+                direction: 'Down',
+                pressed: false
+            });
+        }
+
+        if (e.keyCode === 37 /* left */ ||
+            e.keyCode === 65 /* a */ ||
+            e.keyCode === 81 /* q */ ) {
+            triangleLeft.style.borderColor = "white";
+            left = false;
+            socket.emit('control', {
+                direction: 'Left',
+                pressed: false
+            });
+        }
+    });
 }
--- html/js/script.js
+++ html/js/script.js
@@ -1,197 +1,198 @@
 let goForward = false,
-	goBackwards = false,
-	turnLeft = false,
-	turnRight = false;
+    goBackwards = false,
+    turnLeft = false,
+    turnRight = false;
 
-let socket = io.connect('http://192.168.43.117');
+let socket = io.connect('http://localhost');
 
 socket.on('update', function (data) {
-	switch (data.direction) {
-		case 'Up':
-			goForward = data.pressed;
-			break;
-		case 'Down':
-			goBackwards = data.pressed;
-			break;
-		case 'Left':
-			turnLeft = data.pressed;
-			break;
-		case 'Right':
-			turnRight = data.pressed;
-			break;
-	}
+    switch (data.direction) {
+        case 'Up':
+            goForward = data.pressed;
+            break;
+        case 'Down':
+            goBackwards = data.pressed;
+            break;
+        case 'Left':
+            turnLeft = data.pressed;
+            break;
+        case 'Right':
+            turnRight = data.pressed;
+            break;
+    }
 });
 
 AFRAME.registerComponent("rover-controls", {
 
-	init: function () {
-		let world = document.querySelector("a-scene").systems.physics.driver.world;
+    init: function () {
+        let world = document.querySelector("a-scene").systems.physics.driver.world;
 
-		// Create materials
-		var groundMaterial = new CANNON.Material("groundMaterial");
-		var roverMaterial = new CANNON.Material("roverMaterial");
+        // Create materials
+        var groundMaterial = new CANNON.Material("groundMaterial");
+        var roverMaterial = new CANNON.Material("roverMaterial");
 
-		var slipperyContactMaterial = new CANNON.ContactMaterial(groundMaterial, roverMaterial, {
-			friction: 0.0,
-			restitution: 0.3,
-			contactEquationStiffness: 1e8,
-			contactEquationRelaxation: 3
-		});
+        var slipperyContactMaterial = new CANNON.ContactMaterial(groundMaterial, roverMaterial, {
+            friction: 0.0,
+            restitution: 0.3,
+            contactEquationStiffness: 1e8,
+            contactEquationRelaxation: 3
+        });
 
-		// The ContactMaterials must be added to the world
-		world.addContactMaterial(slipperyContactMaterial);
+        // The ContactMaterials must be added to the world
+        world.addContactMaterial(slipperyContactMaterial);
 
-		this.el.addEventListener("body-loaded", function (body) {
-			// Apply materials to entity and ground
-			this.body.material = roverMaterial;
+        this.el.addEventListener("body-loaded", function (body) {
+            // Apply materials to entity and ground
+            this.body.material = roverMaterial;
 
-			let ground = document.getElementById("ground");
-			ground.body.material = groundMaterial;
+            let ground = document.getElementById("ground");
+            ground.body.material = groundMaterial;
 
-			this.body.addEventListener("collide", function (e) {
+            this.body.addEventListener("collide", function (e) {
 
-				if (e.body.el.id === "landscape") {
-					console.log("COLLISION DETECTED WITH LANDSCAPE!");
-					roverHealth = roverHealth - 1;
-				} else {
-					console.log("COLLISION DETECTED! Body: ", e.body.el, " ID: ", e.body.el.id);
-				}
-			});
+                if (e.body.el.id === "landscape") {
+                    console.log("COLLISION DETECTED WITH LANDSCAPE!");
+                    roverHealth = roverHealth - 1;
+                } else {
+                    console.log("COLLISION DETECTED! Body: ", e.body.el, " ID: ", e.body.el.id);
+                }
+            });
 
-			let sand = document.querySelectorAll("#sand");
-			for (i = 0; i < sand.length; i++) {
-				sand[i].body.collisionResponse = false;
-			}
+            let sand = document.querySelectorAll("#sand");
+            for (i = 0; i < sand.length; i++) {
+                sand[i].body.collisionResponse = false;
+            }
 
-			let rocks = document.querySelectorAll("#rocks");
-			for (i = 0; i < rocks.length; i++) {
-				rocks[i].body.collisionResponse = false;
-			}
+            let rocks = document.querySelectorAll("#rocks");
+            for (i = 0; i < rocks.length; i++) {
+                rocks[i].body.collisionResponse = false;
+            }
 
-			requestAnimationFrame(gameLoop)
-		});
-	},
+            requestAnimationFrame(gameLoop)
+        });
+    },
 });
 
 document.addEventListener('keydown', press)
+
 function press(e) {
-	if (e.keyCode === 38 /* up */ || e.keyCode === 87 /* w */ || e.keyCode === 90 /* z */)
-		goForward = true;
-	if (e.keyCode === 39 /* right */ || e.keyCode === 68 /* d */)
-		turnRight = true;
-	if (e.keyCode === 40 /* down */ || e.keyCode === 83 /* s */)
-		goBackwards = true;
-	if (e.keyCode === 37 /* left */ || e.keyCode === 65 /* a */ || e.keyCode === 81 /* q */)
-		turnLeft = true;
+    if (e.keyCode === 38 /* up */ || e.keyCode === 87 /* w */ || e.keyCode === 90 /* z */ )
+        goForward = true;
+    if (e.keyCode === 39 /* right */ || e.keyCode === 68 /* d */ )
+        turnRight = true;
+    if (e.keyCode === 40 /* down */ || e.keyCode === 83 /* s */ )
+        goBackwards = true;
+    if (e.keyCode === 37 /* left */ || e.keyCode === 65 /* a */ || e.keyCode === 81 /* q */ )
+        turnLeft = true;
 }
 
 document.addEventListener('keyup', release)
+
 function release(e) {
-	if (e.keyCode === 38 /* up */ || e.keyCode === 87 /* w */ || e.keyCode === 90 /* z */)
-		goForward = false;
-	if (e.keyCode === 39 /* right */ || e.keyCode === 68 /* d */)
-		turnRight = false;
-	if (e.keyCode === 40 /* down */ || e.keyCode === 83 /* s */)
-		goBackwards = false;
-	if (e.keyCode === 37 /* left */ || e.keyCode === 65 /* a */ || e.keyCode === 81 /* q */)
-		turnLeft = false;
+    if (e.keyCode === 38 /* up */ || e.keyCode === 87 /* w */ || e.keyCode === 90 /* z */ )
+        goForward = false;
+    if (e.keyCode === 39 /* right */ || e.keyCode === 68 /* d */ )
+        turnRight = false;
+    if (e.keyCode === 40 /* down */ || e.keyCode === 83 /* s */ )
+        goBackwards = false;
+    if (e.keyCode === 37 /* left */ || e.keyCode === 65 /* a */ || e.keyCode === 81 /* q */ )
+        turnLeft = false;
 }
 
 const localForward = new CANNON.Vec3(0, 0, -1);
 const localUp = new CANNON.Vec3(0, 1, 0);
-const linSpeed = 0.1;   // [units] / [s]
-const angSpeed = 0.1;   // [units] / [s]
-const maxLinSpeed = 2.65;  // [units] / [s]
-const maxAngSpeed = 0.70;  // [units] / [s]
+const linSpeed = 0.1; // [units] / [s]
+const angSpeed = 0.1; // [units] / [s]
+const maxLinSpeed = 2.65; // [units] / [s]
+const maxAngSpeed = 0.70; // [units] / [s]
 
-let roverHealth = 100;  // [hp]
+let roverHealth = 100; // [hp]
 let roverBattery = 100; // [bp]
 
 let lastUpdate = Date.now();
 
 function gameLoop() {
 
-	// Update delta time
-	const now = Date.now();
-	const dt = now - lastUpdate;  // [ms]
-	lastUpdate = now;
+    // Update delta time
+    const now = Date.now();
+    const dt = now - lastUpdate; // [ms]
+    lastUpdate = now;
 
-	// Retrieve rover physics body
-	let roverBody = document.getElementById("rover").body;
+    // Retrieve rover physics body
+    let roverBody = document.getElementById("rover").body;
 
-	// Compute world delta vectors for linear and angualar velocities
-	const localLinDelta = localForward.mult(linSpeed);
-	const localAngDelta = localUp.mult(angSpeed);
-	const worldLinDelta = roverBody.quaternion.vmult(localLinDelta);
-	const worldAngDelta = roverBody.quaternion.vmult(localAngDelta);
+    // Compute world delta vectors for linear and angualar velocities
+    const localLinDelta = localForward.mult(linSpeed);
+    const localAngDelta = localUp.mult(angSpeed);
+    const worldLinDelta = roverBody.quaternion.vmult(localLinDelta);
+    const worldAngDelta = roverBody.quaternion.vmult(localAngDelta);
 
-	// If the battery is not dead, update the rover's velocities
-	if (roverBattery > 0) {
-		if (goForward) {
-			roverBody.velocity.x += worldLinDelta.x;
-			roverBody.velocity.y += worldLinDelta.y;
-			roverBody.velocity.z += worldLinDelta.z;
-		} else if (goBackwards) {
-			roverBody.velocity.x -= worldLinDelta.x;
-			roverBody.velocity.y -= worldLinDelta.y;
-			roverBody.velocity.z -= worldLinDelta.z;
-		} else if (turnLeft) {
-			roverBody.angularVelocity.x += worldAngDelta.x;
-			roverBody.angularVelocity.y += worldAngDelta.y;
-			roverBody.angularVelocity.z += worldAngDelta.z;
-		} else if (turnRight) {
-			roverBody.angularVelocity.x -= worldAngDelta.x;
-			roverBody.angularVelocity.y -= worldAngDelta.y;
-			roverBody.angularVelocity.z -= worldAngDelta.z;
-		}
-	}
+    // If the battery is not dead, update the rover's velocities
+    if (roverBattery > 0) {
+        if (goForward) {
+            roverBody.velocity.x += worldLinDelta.x;
+            roverBody.velocity.y += worldLinDelta.y;
+            roverBody.velocity.z += worldLinDelta.z;
+        } else if (goBackwards) {
+            roverBody.velocity.x -= worldLinDelta.x;
+            roverBody.velocity.y -= worldLinDelta.y;
+            roverBody.velocity.z -= worldLinDelta.z;
+        } else if (turnLeft) {
+            roverBody.angularVelocity.x += worldAngDelta.x;
+            roverBody.angularVelocity.y += worldAngDelta.y;
+            roverBody.angularVelocity.z += worldAngDelta.z;
+        } else if (turnRight) {
+            roverBody.angularVelocity.x -= worldAngDelta.x;
+            roverBody.angularVelocity.y -= worldAngDelta.y;
+            roverBody.angularVelocity.z -= worldAngDelta.z;
+        }
+    }
 
-	// TODO: move to initialization
-	roverBody.linearDamping = 0.9;
-	roverBody.angularDamping = 0.9999;
+    // TODO: move to initialization
+    roverBody.linearDamping = 0.9;
+    roverBody.angularDamping = 0.9999;
 
-	// Set initial health and battery drain factors
-	let healthDrain = 0;    // [hp] / [s]
-	let batteryDrain = 0.5;  // [bp] / [s]
+    // Set initial health and battery drain factors
+    let healthDrain = 0; // [hp] / [s]
+    let batteryDrain = 0.5; // [bp] / [s]
 
-	// Check if the rover is in contact with different materials
-	let contacts = document.querySelector("a-scene").systems.physics.driver.world.contacts;
-	for (i = 0; i < contacts.length; i++) {
-		let contact = contacts[i].bi.el.id;
-		if (contact === "sand") {
-			batteryDrain = 3;
-		}
-		if (contact === "rocks") {
-			healthDrain = 5;
-		}
-	}
+    // Check if the rover is in contact with different materials
+    let contacts = document.querySelector("a-scene").systems.physics.driver.world.contacts;
+    for (i = 0; i < contacts.length; i++) {
+        let contact = contacts[i].bi.el.id;
+        if (contact === "sand") {
+            batteryDrain = 3;
+        }
+        if (contact === "rocks") {
+            healthDrain = 5;
+        }
+    }
 
-	// Compute linear and angular speeds [units] / [s]
-	const curLinSpeed = roverBody.velocity.length();
-	const curAngSpeed = roverBody.angularVelocity.length();
+    // Compute linear and angular speeds [units] / [s]
+    const curLinSpeed = roverBody.velocity.length();
+    const curAngSpeed = roverBody.angularVelocity.length();
 
-	// Compute normalized factors [constant]
-	const linFactor = Math.floor(curLinSpeed / maxLinSpeed * 100) / 100;
-	const angFactor = Math.floor(curAngSpeed / maxAngSpeed * 100) / 100;
+    // Compute normalized factors [constant]
+    const linFactor = Math.floor(curLinSpeed / maxLinSpeed * 100) / 100;
+    const angFactor = Math.floor(curAngSpeed / maxAngSpeed * 100) / 100;
 
-	// Drain health and battery
-	roverHealth -= angFactor * healthDrain * dt / 1000;   // [hp]
-	roverBattery -= linFactor * batteryDrain * dt / 1000;  // [bp]
-	roverBattery -= 0.1 * dt / 1000;  // [bp]
+    // Drain health and battery
+    roverHealth -= angFactor * healthDrain * dt / 1000; // [hp]
+    roverBattery -= linFactor * batteryDrain * dt / 1000; // [bp]
+    roverBattery -= 0.1 * dt / 1000; // [bp]
 
-	// Makes sure health and battery levels don't go below 0
-	roverHealth = Math.max(roverHealth, 0);
-	roverBattery = Math.max(roverBattery, 0);
+    // Makes sure health and battery levels don't go below 0
+    roverHealth = Math.max(roverHealth, 0);
+    roverBattery = Math.max(roverBattery, 0);
 
-	requestAnimationFrame(gameLoop)
+    requestAnimationFrame(gameLoop)
 }
 
 window.setInterval(function () {
-	socket.emit(
-		'roverData',
-		{
-			health: roverHealth,
-			battery: roverBattery
-		}
-	);
+    socket.emit(
+        'roverData', {
+            health: roverHealth,
+            battery: roverBattery
+        }
+    );
 }, 50);
